ff3ce96b7ee0d44827c9ad4f6898b2d0b9563983
Mosè Giordano <m.giordano@ucl.ac.uk>
2019-12-07 23:00:54 +0000

    Test FFMPEG on AppVeyor

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,18 @@
 using DependencyWalker
 using Test
-using ObjectFile, Pango_jll
+using ObjectFile, FFMPEG_jll
 
 @testset "DependencyWalker.jl" begin
-    pango = Library(Pango_jll.libpango_path)
-    @show pango
+    @show FFMPEG_jll.ffmpeg_path
+    io = open(FFMPEG_jll.ffmpeg_path, "r")
+    @show readmeta(io)
+    close(io)
+    ffmpeg_oh = Library(FFMPEG_jll.ffmpeg_path)
+    @show ffmpeg_oh
     if Sys.islinux() && Sys.WORD_SIZE == 32
-        @test_broken pango isa Library{<:ObjectHandle}
+        @test_broken ffmpeg_oh isa Library{<:ObjectHandle}
     else
-        @test pango isa Library{<:ObjectHandle}
+        @test ffmpeg_oh isa Library{<:ObjectHandle}
     end
     @test Library("this does not exist.foo") isa Library{Missing}
 end
