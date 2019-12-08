75a48d9358a697441e67936108904d76e880aca0
Mosè Giordano <m.giordano@ucl.ac.uk>
2019-12-08 00:45:53 +0000

    Test with gdk-pixbuf-query-loaders executable

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,9 @@
 using DependencyWalker
 using Test
-using ObjectFile, Pango_jll
+using ObjectFile, gdk_pixbuf_jll
 
 @testset "DependencyWalker.jl" begin
-    pango = Library(Pango_jll.libpango_path)
+    pango = Library(gdk_pixbuf_jll.gdk_pixbuf_query_loaders_path)
     @show pango
     if Sys.islinux() && Sys.WORD_SIZE == 32
         @test_broken pango isa Library{<:ObjectHandle}
