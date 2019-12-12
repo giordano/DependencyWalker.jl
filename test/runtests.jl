b6fbc3e584408b42f83533692167519bdd09174d
Mosè Giordano <m.giordano@ucl.ac.uk>
2019-12-12 00:53:53 +0000

    Reproduce issue on Windows

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,7 @@ using Test
 using ObjectFile, gdk_pixbuf_jll
 
 @testset "DependencyWalker.jl" begin
+    @show readmeta(open(gdk_pixbuf_jll.gdk_pixbuf_query_loaders_path, "r"))
     pango = Library(gdk_pixbuf_jll.gdk_pixbuf_query_loaders_path)
     @show pango
     if Sys.islinux() && Sys.WORD_SIZE == 32
