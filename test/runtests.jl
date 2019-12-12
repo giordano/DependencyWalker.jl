f3f5066588a15a019f3d03bad02c4bb5a8cbd255
Mosè Giordano <m.giordano@ucl.ac.uk>
2019-12-12 18:27:25 +0000

    Only use SymEngine_jll on Windows

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,18 +1,16 @@
-using DependencyWalker
-using Test
-using ObjectFile, SymEngine_jll
+using SymEngine_jll
 
-@testset "DependencyWalker.jl" begin
-    @show SymEngine_jll.libsymengine_path
-    io = open(SymEngine_jll.libsymengine_path, "r")
-    @show readmeta(io)
-    close(io)
-    libsymengine_oh = Library(SymEngine_jll.libsymengine_path)
-    @show libsymengine_oh
-    # if Sys.islinux() && Sys.WORD_SIZE == 32
-    #     @test_broken libsymengine_oh isa Library{<:ObjectHandle}
-    # else
-    #     @test libsymengine_oh isa Library{<:ObjectHandle}
-    # end
-    # @test Library("this does not exist.foo") isa Library{Missing}
-end
+# @testset "DependencyWalker.jl" begin
+#     @show SymEngine_jll.libsymengine_path
+#     io = open(SymEngine_jll.libsymengine_path, "r")
+#     @show readmeta(io)
+#     close(io)
+#     libsymengine_oh = Library(SymEngine_jll.libsymengine_path)
+#     @show libsymengine_oh
+#     # if Sys.islinux() && Sys.WORD_SIZE == 32
+#     #     @test_broken libsymengine_oh isa Library{<:ObjectHandle}
+#     # else
+#     #     @test libsymengine_oh isa Library{<:ObjectHandle}
+#     # end
+#     # @test Library("this does not exist.foo") isa Library{Missing}
+# end
