0f7b2dbd43efe4f81ee31a9fa7400b7a00acf500
Mosè Giordano <m.giordano@ucl.ac.uk>
2019-12-12 18:09:42 +0000

    Test SymEngine

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,18 +1,18 @@
 using DependencyWalker
 using Test
-using ObjectFile, FFMPEG_jll
+using ObjectFile, SymEngine_jll
 
 @testset "DependencyWalker.jl" begin
-    @show FFMPEG_jll.ffmpeg_path
-    io = open(FFMPEG_jll.ffmpeg_path, "r")
+    @show SymEngine_jll.libsymengine_path
+    io = open(SymEngine_jll.libsymengine_path, "r")
     @show readmeta(io)
     close(io)
-    ffmpeg_oh = Library(FFMPEG_jll.ffmpeg_path)
-    @show ffmpeg_oh
+    libsymengine_oh = Library(SymEngine_jll.libsymengine_path)
+    @show libsymengine_oh
     if Sys.islinux() && Sys.WORD_SIZE == 32
-        @test_broken ffmpeg_oh isa Library{<:ObjectHandle}
+        @test_broken libsymengine_oh isa Library{<:ObjectHandle}
     else
-        @test ffmpeg_oh isa Library{<:ObjectHandle}
+        @test libsymengine_oh isa Library{<:ObjectHandle}
     end
     @test Library("this does not exist.foo") isa Library{Missing}
 end
