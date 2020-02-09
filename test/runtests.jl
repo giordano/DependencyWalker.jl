7089817c43613c1792d55852c059422a10fc187a
Mosè Giordano <m.giordano@ucl.ac.uk>
2020-02-09 22:04:15 +0000

    Test opening SuiteSparse

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,14 @@
 using DependencyWalker
 using Test
-using ObjectFile, Pango_jll
+using ObjectFile, SuiteSparse_jll
 
-@testset "DependencyWalker.jl" begin
-    pango = Library(Pango_jll.libpango_path)
-    @show pango
-    if Sys.islinux() && Sys.WORD_SIZE == 32
-        @test_broken pango isa Library{<:ObjectHandle}
-    else
-        @test pango isa Library{<:ObjectHandle}
-    end
-    @test Library("this does not exist.foo") isa Library{Missing}
-end
+# @testset "DependencyWalker.jl" begin
+#     pango = Library(Pango_jll.libpango_path)
+#     @show pango
+#     if Sys.islinux() && Sys.WORD_SIZE == 32
+#         @test_broken pango isa Library{<:ObjectHandle}
+#     else
+#         @test pango isa Library{<:ObjectHandle}
+#     end
+#     @test Library("this does not exist.foo") isa Library{Missing}
+# end
