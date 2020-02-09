944a4558b5a65db695cefedc85b9a686d3336184
Mosè Giordano <m.giordano@ucl.ac.uk>
2020-02-09 22:11:16 +0000

    Try walking into the library

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,16 @@
 using DependencyWalker
 using Test
-using ObjectFile, SuiteSparse_jll
+using ObjectFile, Pkg.BinaryPlatforms
+
+@show platform_key_abi()
+
+if Sys.iswindows()
+    if haskey(ENV, "TRAVIS")
+        Library(raw"C:\Users\travis\.julia\artifacts\e177c77afc4dbd274277508bd5d069731bbe517e\bin\libspqr.dll")
+    else
+        Library(raw"C:\Users\appveyor\.julia\artifacts\e082284e0a29a950b24a5a1817e7eef7247ccb7c\bin\libspqr.dll")
+    end
+end
 
 # @testset "DependencyWalker.jl" begin
 #     pango = Library(Pango_jll.libpango_path)
