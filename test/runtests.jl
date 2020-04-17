89393f0ce14838f8fc03d970a13a12c205239085
Mosè Giordano <mose@gnu.org>
2020-04-17 17:32:07 +0100

    Add libX11 to the game

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,7 +8,8 @@ using libpng_jll
 using Fontconfig_jll
 using FreeType2_jll
 using Bzip2_jll
-using Xorg_libXext_jll
+# using Xorg_libXext_jll
+using Xorg_libX11_jll
 using Xorg_libXrender_jll
 using LZO_jll
 using Zlib_jll
@@ -18,6 +19,10 @@ using Xorg_libxcb_jll
 using Libdl
 
 @show dllist()
+
+println()
+
+@show Library(Xorg_libX11_jll.libX11_path)
 
 println()
 
