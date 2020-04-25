0de1805f2a6c2eed7a88ed676bf009b3809ddce1
Mosè Giordano <mose@gnu.org>
2020-04-25 23:37:34 +0100

    Also Windows 32-bit doesn’t seem to work

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,7 @@ using ObjectFile, Pango_jll
 @testset "DependencyWalker.jl" begin
     pango = Library(Pango_jll.libpango_path)
     @show pango
-    if Sys.islinux() && Sys.WORD_SIZE == 32
+    if (Sys.islinux() || Sys.iswindows()) && Sys.WORD_SIZE == 32
         @test_broken pango isa Library{<:ObjectHandle}
     else
         @test pango isa Library{<:ObjectHandle}
