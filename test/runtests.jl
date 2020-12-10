9aa85f7ce8b58fd32245ed1cb73c94309983e8f6
Mosè Giordano <mose@gnu.org>
2020-12-10 18:40:57 +0000

    Switch CI to GitHub Actions

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,10 +5,6 @@ using ObjectFile, Pango_jll
 @testset "DependencyWalker.jl" begin
     pango = Library(Pango_jll.libpango_path)
     @show pango
-    if (Sys.islinux() || Sys.iswindows()) && Sys.WORD_SIZE == 32
-        @test_broken pango isa Library{<:ObjectHandle}
-    else
-        @test pango isa Library{<:ObjectHandle}
-    end
+    @test pango isa Library{<:ObjectHandle}
     @test Library("this does not exist.foo") isa Library{Missing}
 end
